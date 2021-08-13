942677a2b40c14c9fe6ac758bad5ed917512683c
Admin <tmstudentler@gmail.com>
2021-08-13 19:02:31 +0300

    step6

---
+++ app/src/main/java/com/example/api_all/ListView_ArrayAdapter/Example8/SpinnerBigActivity.java
@@ -0,0 +1,51 @@
+package com.example.api_all.ListView_ArrayAdapter.Example8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.api_all.R;
+
+public class SpinnerBigActivity extends AppCompatActivity {
+
+    String[] countries = {"out", "extends", "life", "say", "said", "much", "codes", "eye"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner_big);
+
+        TextView selection = findViewById(R.id.selection);
+
+        Spinner spinner = findViewById(R.id.spinner2);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, countries);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)parent.getItemAtPosition(position);
+                selection.setText(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        };
+
+        spinner.setOnItemClickListener(itemSelectedListener);
+
+
+    }
+}
